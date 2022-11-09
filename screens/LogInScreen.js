8a6894e79c9ee85a38f8ba023c2e53a041b803b5
Rattikan Baimiden <rattikanbaimiden@gmail.com>
2022-11-10 01:20:02 +0700

    Update Log in and Sign Up Screen

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -1,32 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView,StyleSheet, Text, View,TextInput} from 'react-native'
 import React, { useState, useEffect } from 'react'
-import { TextInput, Button } from 'react-native-paper'
+import { Button } from 'react-native-paper'
 
 const LogInScreen = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
     return (
-        <View>
-            <Text>Login</Text>
-            <TextInput
-            label="Username"
-            value={username}
-            onChangeText={text => setUsername(text)}
-            />
-            <TextInput
-            label="Password"
-            secureTextEntry
-            right={<TextInput.Icon icon="eye" />}
-            value={password}
-            onChangeText={text => setPassword(text)}
-            />
-            <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}> Press me </Button>
+        <SafeAreaView style={styles.container}>
+            <View style={{paddingBottom: 35,}}>
+                <Text style={styles.footer}>Log In</Text>
+                <Text style={styles.tap}>Username</Text>
+                <TextInput
+                    style={[styles.input, styles.bg]}
+                    onChangeText={text => setUsername(text)}
+                    value={username}
+                />
+                <Text style={styles.tap}>Password</Text>
+                <TextInput
+                    secureTextEntry
+                    style={[styles.input, styles.bg]}
+                    onChangeText={text => setPassword(text)}
+                    value={password}
+                />
+            </View>
 
-        </View>
+            <Button style={styles.buttonContainer} mode="contained" onPress={() => console.log('Pressed')}> Log In </Button>
+
+        </SafeAreaView>
     )
 }
 
 export default LogInScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        padding: 10,
+        borderRadius: 50,
+        alignSelf: "center",
+        width: '100%',
+        maxWidth: 340,
+    },
+    tap: {
+        width: '100%',
+        maxWidth: 340,
+        alignSelf: "center",
+        justifyContent: "center"
+    },
+    footer: {
+        color: '#000',
+        fontSize: 40,
+        fontWeight: 'bold',
+        paddingBottom: 35,
+        paddingLeft: 20
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#ACC5F8',
+        width: '100%',
+        justifyContent: "center",
+    },
+    bg: {
+        backgroundColor: '#ffff'
+    },
+    buttonContainer: {
+        alignSelf: "center",
+        width: '100%',
+        maxWidth: 340,
+        backgroundColor: '#4F4F4F',
+    }
+})
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,14 +1,87 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView,StyleSheet, Text, View,TextInput} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Button } from 'react-native-paper'
+
 
 const SignUpScreen = () => {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [password, setPassword] = useState("")
   return (
-    <View>
-      <Text>SignUpScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <View style={{paddingBottom: 35}}>
+        <Text style={styles.footer}>Sign Up</Text>
+        <Text style={styles.tap}>Username</Text>
+        <TextInput
+          style={[styles.input, styles.bg]}
+          onChangeText={text => setUsername(text)}
+          value={username}
+        />
+        <Text style={styles.tap}>Email</Text>
+        <TextInput
+          style={[styles.input, styles.bg]}
+          onChangeText={text => setEmail(text)}
+          value={email}
+        />
+        <Text style={styles.tap}>Phone Number</Text>
+        <TextInput
+          style={[styles.input, styles.bg]}
+          onChangeText={text => setPhoneNumber(text)}
+          value={phoneNumber}
+        />
+        <Text style={styles.tap}>Password</Text>
+        <TextInput
+          style={[styles.input, styles.bg]}
+          onChangeText={text => setPassword(text)}
+          value={password}
+        />
+      </View>
+
+      <Button style={styles.buttonContainer} mode="contained" onPress={() => console.log('Pressed')}> Sign Up </Button>
+
+    </SafeAreaView>
+    )
 }
 
 export default SignUpScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderRadius: 50,
+    alignSelf: "center",
+    width: '100%',
+    maxWidth: 340,
+  },
+  tap: {
+    width: '100%',
+    maxWidth: 340,
+    alignSelf: "center",
+    justifyContent: "center"
+  },
+  footer: {
+    color: '#000',
+    fontSize: 40,
+    fontWeight: 'bold',
+    paddingBottom: 35,
+    paddingTop: 35,
+    paddingLeft: 20,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#ACC5F8'
+  },
+  bg: {
+    backgroundColor: '#ffff'
+  },
+  buttonContainer: {
+    alignSelf: "center",
+    width: '100%',
+    maxWidth: 340,
+    backgroundColor: '#4F4F4F',
+  }
+ 
+})
